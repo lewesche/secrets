2369fd557587e82155e3a360b5c792de9e3096fe
Leif Wesche <wesche@usc.edu>
2021-01-15 21:37:42 -0800

    added test code for homebrew formula

--- src/main.cpp
+++ src/main.cpp
@@ -186,7 +186,11 @@ void get_targets(string& target_tag, int& target_idx) {
 	}
 }
 
-int main() {
+int main(int argc, char **argv) {
+	if(argc > 1)
+		if(!strcmp(argv[1], "test"))
+			return 0;
+
 	print_intro();
 	string homepath = getenv("HOME");
 	string fname(homepath + "/.secret.txt");
