7a01e8eff74e4edcf91eaaa0317c1a4b6129430f
Leif Wesche <wesche@usc.edu>
2021-02-17 17:02:54 -0800

    fixed bug where I forgot to check for unprintable characters in no key case

--- src/baseModule.cpp
+++ src/baseModule.cpp
@@ -57,6 +57,7 @@ vector<secret*> BaseModule::base_read(const string& key, const string& target_ta
                 s->set_idx(i);
 				if(key.empty()) {
 					//no need to decode
+					s->check_dec();
 					s->set_dec(s->get_enc());
 				} else {
 					jobs.push(s);
