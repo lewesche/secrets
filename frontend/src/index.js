c65d41fc6e921fe8e26240590fe689ddeae4c424
Leif Wesche <wesche@usc.edu>
2021-03-07 19:34:06 -0800

    Webassembly is running on the front end! Not deployed yet

---
+++ frontend/src/index.js
@@ -0,0 +1,262 @@
+module.exports = {
+	init, 
+	clicked_new_user,
+	clicked_go, 
+	wasm_test
+}
+
+const rust = import('../pkg');
+const MAX_LEN = 4096;
+
+function wasm_test() {
+	rust.then(m => {
+		console.log('checksum: {}', m.get_checksum('username', 'password'))
+		let enc = m.encode("username", "password", "hello")
+		console.log('enc: {}', enc)
+		let dec = m.decode("username", "password", enc)
+		dec = String.fromCharCode(...dec)
+		console.log('dec, {}', dec)
+  }).catch(console.error);
+}
+
+function init() {
+    let about = document.getElementById('about');
+        let title = document.createElement("h2");
+
+        title.innerHTML = "- About";
+        title.classList.add("left", "dropDown");
+        about.append(title);
+
+    let inner = document.createElement("div");
+    
+    let p1 = document.createElement("p");
+    p1.innerHTML = "Secrets is an app that lets you store strings like passwords in a secure way."
+    inner.append(p1);
+
+    let p2 = document.createElement("p");
+    p2.innerHTML = "No passwords are saved, and no data is saved in an unencrypted format."
+    inner.append(p2);   
+    
+    let p3 = document.createElement("p");
+    p3.innerHTML = "Optionally, secrets can be created with a tag."
+    inner.append(p3);   
+
+    let p4 = document.createElement("p");
+    p4.innerHTML = "Tags, along with the index, can be used to filter read/delete operations. "
+    inner.append(p4);   
+
+    let diagram = document.createElement("img");
+    diagram.src="img/secrets_diagram_crop.png";
+    diagram.width = "950";
+    inner.append(diagram);
+
+
+        about.append(inner);
+
+    $(".dropDown").click(function(){
+        let curr = this.innerHTML;
+        if(curr[0] == '-') {
+            curr = curr.replace('-', '+');
+        } else if (curr[0] == '+') {
+            curr = curr.replace('+', '-');
+        }   
+        this.innerHTML = curr;
+        var oldWidth = $(this.nextSibling).width();
+        $(this.nextSibling).slideToggle(200, () => {$(this).width(oldWidth);} );
+    }); 
+
+}
+
+function clicked_new_user() {
+    clear();
+    usr = document.getElementById('usr_txt').value;
+    pwd = document.getElementById('pwd_txt').value;
+
+    body = {};
+    body.action = "c";
+    body.usr = usr;
+    if(pwd != "")
+        body.pwd = pwd;
+
+    if (JSON.stringify(body).length > MAX_LEN) {
+        clear();
+        setError("Request is too long")
+        return;
+    }
+
+
+    let url = "/secrets/new";
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xhr.onload = function (e) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+            console.log(xhr.responseText);
+            let obj = JSON.parse(xhr.responseText);
+            if(obj.success == "true") {
+                setNotify("New user " + usr + " created")
+            } else if(obj.hasOwnProperty("e")) {
+                setError(obj.e);
+            } else {
+                setError("Failed with unknown error");
+            }
+            } else {
+            setError("HTTP error: " + xhr.statusText);
+            }
+        }
+    };
+    xhr.onerror = function (e) {
+        setError("HTTP error: " + xhr.statusText);
+    };
+    xhr.send(JSON.stringify(body));
+}
+
+function clicked_go() {
+    clear();
+    let radios = document.getElementsByName('action_button');
+    for(let i=0; i<radios.length; i++) {
+        if(radios[i].checked) {
+            send_query(radios[i].value)
+            return;
+        }
+    }
+}
+
+function send_query(action) {
+    idx = document.getElementById('idx_txt').value;
+    tag = document.getElementById('tag_txt').value;
+    usr = document.getElementById('usr_txt').value;
+    dec = document.getElementById('dec_txt').value;
+    pwd = document.getElementById('pwd_txt').value;
+
+    body = {};
+
+    body.action = action;
+
+    if(action=="w")
+        if(dec !="")
+            body.data = dec;
+
+    if(usr!="")
+        body.usr = usr;
+
+    if(pwd != "")
+        body.pwd = pwd;
+
+    if(tag!="")
+        body.tag = tag;
+
+    if(action!="w" && idx!="")
+        body.idx = idx;
+
+    clear_fields();
+
+    if (JSON.stringify(body).length > MAX_LEN) {
+        setError("Request is too long")
+        return;
+    }
+
+    request(body);
+}
+
+function request(body) {
+    let url = "/secrets/usr";
+    console.log(body);
+
+    let xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+    xhr.onload = function (e) {
+        if (xhr.readyState === 4) {
+            if (xhr.status === 200) {
+                try {
+                    console.log(xhr.responseText);
+                    let obj = JSON.parse(xhr.responseText);
+                    if(obj.success=="true") {
+                        if(body.action == "r") {
+                            setTable(obj.res);
+                        } else {
+                            // Read again to get an updated table
+                            send_query("r");
+                        }
+                    } else if(obj.hasOwnProperty("e")) {
+                        setError(obj.e);
+                    } else {
+                        setError("Unknown Error");
+                    }
+                } catch(e) { // valid delete and write responses return invalid json - prompting a recursive read
+                    setError(e);
+                    console.log("bad json: " + xhr.responseText + e);
+                }
+            } else {
+            setError("HTTP error: " + xhr.statusText);
+            }
+        }
+    };
+    xhr.onerror = function (e) {
+        console.error(xhr.statusText);
+        setError("HTTP error: " + xhr.statusText);
+    };
+    xhr.send(JSON.stringify(body));
+}
+
+function setNotify(str) {
+    ele = document.getElementById("notify");
+    ele.style.display = "block";
+    ele.innerHTML = str;
+}
+
+
+function setError(e) {
+    ele = document.getElementById("error");
+    ele.style.display = "block";
+    ele.innerHTML = e;
+}
+
+function setTable(obj) {
+    console.log("in set table");
+    html_txt = "<table>";
+    html_txt += "<tr>" + "<th>idx</th>" + "<th>tag</th>" + "<th>text</th>" + "</tr>";
+    for(i=0; i<obj.length; i++) {
+        html_txt += "<tr>"
+        html_txt += "<td>" + i + "</td>";
+        html_txt += "<td>";
+        if(obj[i].hasOwnProperty('tag')){
+            html_txt += obj[i].tag;
+        }
+        html_txt += "</td>";
+
+        html_txt += "<td>" + obj[i].enc + "</td>";
+        html_txt += "</tr>"
+    }
+    html_txt += "</table>";
+    document.getElementById("table_div").innerHTML = html_txt;
+}
+
+function clear() {
+    clearError();
+    clearTable();
+    clearNotify();
+}
+
+function clearNotify() {
+    document.getElementById("notify").style.display = "none";
+}
+
+function clearError() {
+    document.getElementById("error").style.display = "none";
+}
+
+
+function clearTable() {
+    document.getElementById("table_div").innerHTML = "";
+}
+
+function clear_fields() {
+    document.getElementById('idx_txt').value = "";
+    document.getElementById('tag_txt').value = "";
+    document.getElementById('dec_txt').value = "";
+}
+
---
+++ frontend/src/lib.rs
@@ -0,0 +1,108 @@
+use wasm_bindgen::prelude::*;
+
+use rand::prelude::*;
+use std::num::Wrapping;
+
+// These functions are used to generate a 
+// "checksum" value from a two strings (ie
+// username + password)
+
+// Strings should be at least 3 char long
+// "checksum" values are derived from two steps:
+
+// 1) Add/multiply elements with overflow
+// into a "hash". This seems to yield pretty
+// random results, ie strings with the same
+// straight forward additive sum will likely
+// have a very different a "hash".
+// However it's determinate and reversible.
+pub fn hash_strings(s1: &String, s2: &String) -> u64 {
+    let s1 = s1.as_bytes();
+
+    let mut v1: Vec<Wrapping<u64>> = Vec::new(); 
+    for i in 0..s1.len() {
+        v1.push(Wrapping(s1[i].into()));
+    }   
+
+    let s2 = s2.as_bytes();
+    let mut v2: Vec<Wrapping<u64>> = Vec::new(); 
+    for i in 0..s2.len() {
+        v2.push(Wrapping(s2[i].into()));
+    }   
+    
+    let mut hash = Wrapping(123u64);
+    // 110 is what I want because it's right in the middle of lower case chars in ascii
+    let mut prev = Wrapping(110); 
+
+    for e1 in v1.iter() {
+        if e1 >= &prev {
+            hash = hash * e1; 
+        } else {
+            hash = hash + e1; 
+        }   
+        prev = hash%Wrapping(255u64);
+        if prev>=Wrapping(20u64) {prev -= Wrapping(20u64);}
+        for e2 in v2.iter() {
+            if e2 >= &prev {
+                hash = hash * e2; 
+            } else {
+                hash = hash + e2; 
+            }   
+            prev = hash%Wrapping(255u64);
+            if prev>=Wrapping(20u64) {prev -= Wrapping(20u64);}
+        }   
+    }   
+    hash.0
+}
+
+// 2) Use the hash to seed a CSPRNG and take a random value.
+// This random number is the "checksum". I'm no crypto
+// expert, but I think the point of a CSPRNG is that it 
+// isn't feasibly reversible. The Hc128 seems to perform
+// well according to rust documentation
+// https://docs.rs/rand/0.5.0/rand/prng/index.html
+#[wasm_bindgen]
+pub fn get_checksum(s1: String, s2: String) -> u64 {
+    let hash = hash_strings(&s1, &s2);
+    let mut rng = rand_hc::Hc128Rng::seed_from_u64(hash);
+    rng.gen_range(0..u64::MAX)
+}
+
+
+// Functions to encode/decode given a <username, password>
+// Just a caesar cipher with CSPRNG shifts
+#[wasm_bindgen]
+pub fn encode(s1: String, s2: String, dec: String) -> Vec<u8> {
+    // add length so that different secrets for the same user will (often) have different hashes
+    let hash = hash_strings(&s1, &s2) + (dec.len() as u64);
+    let mut rng = rand_hc::Hc128Rng::seed_from_u64(hash);
+
+    let dec = dec.as_bytes();
+    let mut enc: Vec<u8> = Vec::new();
+
+    for i in 0..dec.len() {
+        let c = Wrapping(dec[i]) + Wrapping(rng.gen_range(0..u8::MAX));
+        enc.push(c.0);
+    }
+    enc
+}
+
+#[wasm_bindgen]
+pub fn decode(s1: String, s2: String, enc: &[u8]) -> Vec<u8> {
+    // add length so that different secrets for the same user will (often) have different hashes
+    let hash = hash_strings(&s1, &s2) + (enc.len() as u64);
+    let mut rng = rand_hc::Hc128Rng::seed_from_u64(hash);
+
+    let mut dec: Vec<u8> = Vec::new();
+
+    for i in 0..enc.len() {
+        let c = Wrapping(enc[i]) - Wrapping(rng.gen_range(0..u8::MAX));
+        if c.0<32 || c.0>126 {
+            dec.push(63); // 63=='?'
+        } else {
+            dec.push(c.0);
+        }
+    }
+    dec
+}
+
