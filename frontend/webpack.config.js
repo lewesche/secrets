5afd1a7761e238ee9c7bf0dccf8403aca2cd1dd5
Leif Wesche <wesche@usc.edu>
2021-03-09 20:38:18 -0800

    another webpack config

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -6,7 +6,7 @@ const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 module.exports = {
     entry: './src/index.js',
     output: {
-		publicPath: 'secrets',
+		publicPath: 'secrets.html',
     	path: path.resolve(__dirname, 'dist'),
     	filename: 'index.js',
 		libraryTarget: 'var',
